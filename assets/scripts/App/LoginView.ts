8a6641310041336026b25a0417596976141f7d1e
akeboshi <akeboshi82@gmail.com>
2024-11-04 18:01:45 +0800

    尝试创建socket连接/socket发送登录协议

--- assets/scripts/App/LoginView.ts
+++ assets/scripts/App/LoginView.ts
@@ -7,6 +7,8 @@ export class LoginView extends Component {
     @property(Node)
     loginBtn:Node;
 
+    private socket;
+
     start() {
 
     }
@@ -16,10 +18,30 @@ export class LoginView extends Component {
     }
 
     public startClick(){
+        this.socket = new WebSocket("wss://test.paipai2.xinjiaxianglao.com/api/home");
+        this.socket.onopen = () => {
+            console.log('socket open');
+        }
+        this.socket.onclose = () => {
+            console.log('socket close');
+        }
+        this.socket.onmessage = (data) => {
+            console.log(data);
+        }
+        this.socket.onerror = (err) => {
+            console.log(err);
+        }
         console.log('clicked');
     }
 
     public loginClick(){
+        var json = JSON.stringify({"action": 5, "data": {
+                "request_id": "123456",
+                "username": "user",
+                "password": "e10adc3949ba59abbe56e057f20f883e"
+            }
+        })
+        this.socket.send(json);
         console.log('loginClick');
     }
 }
