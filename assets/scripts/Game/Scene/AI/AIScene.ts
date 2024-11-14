a891df01783211011ef0ef5304d1f5620f646863
miokky <iamtbfeng@outlook.com>
2024-11-14 10:23:20 +0800

    meta files

--- assets/scripts/Game/Scene/AI/AIScene.ts
+++ assets/scripts/Game/Scene/AI/AIScene.ts
@@ -1,9 +1,16 @@
 import {BaseObejct} from "../../../Core/Object/BaseObject";
+import { DebugLog } from "../../../Core/Util/DebugLog";
 
 export class AIScene extends BaseObejct{
        constructor() {
            super();
        }
 
+       protected start(): void {
+           DebugLog.instance.log("AIScene Start");
+       }
 
+       protected update(dt: number): void {
+           
+       }
 }
