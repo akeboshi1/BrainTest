7033af9fd79374dffaf5c6fafe0b35273ee09dd1
akeboshi <akeboshi82@gmail.com>
2024-11-28 05:21:38 +0800

    数字捕鱼主逻辑

---
+++ assets/catchFish/script/Fish.ts
@@ -0,0 +1,85 @@
+import {Prefab,instantiate,Node,Sprite,Label,SpriteFrame,Vec3,Button} from 'cc';
+import {DebugLog} from "db://assets/scripts/Core/Util/DebugLog";
+import {EventManager} from "db://assets/scripts/Core/Manager/Event/EventManager";
+export class Fish {
+    public static FishClick:string="FishClick";
+    private _fishPrefab:Prefab;
+    private _fish:Node;
+    private _fishSprite:Sprite;
+    private _label:Label;
+    private _button:Button;
+
+    private _data:string;
+
+    public currentIndex = -1;
+    public curTween;
+    constructor(prefab:Prefab){
+        this._fishPrefab = prefab;
+        this.create();
+    }
+    create(){
+        this._fish = instantiate(this._fishPrefab);
+        this._fishSprite = this._fish.getChildByName("fish").getComponent(Sprite);
+        this._label = this._fish.getChildByName("label").getComponent(Label);
+        this._button = this._fish.getComponent(Button);
+
+    }
+
+    setParent(parent:Node){
+        this._fish.parent = parent;
+    }
+    
+    setPosition(x:number,y:number){
+        this._fish.setPosition(x,y);
+    }
+
+    setSpriteFrame(spriteFrame:SpriteFrame){
+        if(this._fishSprite){
+            this._fishSprite.spriteFrame = spriteFrame;
+        }
+    }
+
+    setQuestion(question){
+        this._button.node.off('click', this.clickHandler, this);
+        this._button.node.on('click', this.clickHandler, this);
+        this._data = question;
+        this._label.string = question.question;
+        let answers:string[] = question.options;
+        this.currentIndex = answers.indexOf(question.correctAnswer);
+    }
+
+    clickHandler(event: Event) {
+        // 在这里处理点击事件
+        DebugLog.instance.log("点击了鱼，问题为：" + this._data);
+        EventManager.getInstance().emit(Fish.FishClick,this);
+    }
+
+    getData():any{
+        return this._data;
+    }
+
+    get position(){
+        return this._fish.getPosition();
+    }
+
+    get worldPosition(){
+        return this._fish.getWorldPosition();
+    }
+
+    setSelect(color,scale){
+        this._fishSprite.color = color;
+        this.setScale(scale);
+    }
+
+    setScale(_scale:number=1){
+        this._fish.scale = new Vec3(_scale,_scale,_scale);
+    }
+
+    set position(vec3:Vec3){
+        this._fish.setPosition(vec3);
+    }
+
+    destroy():void{
+
+    }
+}
--- assets/catchFish/script/catchfish.ts
+++ assets/catchFish/script/catchfish.ts
@@ -1,5 +1,35 @@
-import { _decorator, Component, Button, Node,Label } from 'cc';
+import { _decorator, Component, Sprite, Node,Label, Prefab,SpriteFrame,tween,Vec3,instantiate } from 'cc';
+import { SceneManager } from '../../scripts/Core/Manager/Scene/SceneManager';
+import { ColorUtil } from '../../scripts/Core/Util/ColorUtil';
+import { Fish } from './Fish';
+import {EventManager} from "db://assets/scripts/Core/Manager/Event/EventManager";
 const { ccclass, property } = _decorator;
+
+
+const questions = [
+    {
+        "question": "5 + 3 = ?",
+        "options": ["8", "9", "7", "6"],
+        "correctAnswer": "8"
+    },
+    {
+        "question": "7 - 2 = ?",
+        "options": ["5", "4", "6", "3"],
+        "correctAnswer": "5"
+    },
+    {
+        "question": "9 + 1 = ?",
+        "options": ["10", "11", "9", "8"],
+        "correctAnswer": "10"
+    },
+    {
+        "question": "4 - 1 = ?",
+        "options": ["3", "2", "5", "4"],
+        "correctAnswer": "3"
+    }
+];
+
+const SHOOT_INTERVAL = 5;
 
 @ccclass('catchfish')
 export class catchfish extends Component {
@@ -18,6 +48,24 @@ export class catchfish extends Component {
     @property(Label)
     Timer:Label;
 
+    @property([Node])
+    wangs:Node[]= [];
+
+    @property([SpriteFrame])
+    spriteFrames:SpriteFrame[]=[];
+
+    @property(Label)
+    catchLabel:Label;
+
+    @property(Node)
+    fishParentNode:Node;
+
+    @property(Prefab)
+    fishPrefab:Prefab;
+
+    @property(Prefab)
+    wangPrefab:Prefab;
+
     // @property(Button)
     // startBtn:Button;
 
@@ -25,8 +73,15 @@ export class catchfish extends Component {
     // failBtn:Button;
 
 
+    private selectColor = ColorUtil.hexToColor("#3AEB0E");
+    private unSelectColor = ColorUtil.hexToColor("#FFFFFF");
+
+    private fishs:Fish[];
+    private _curFish:Fish;
+
     start() {
         this.gameBeforeView.active = true;
+        this.fishs = [];
     }
 
     startGame(){
@@ -34,11 +89,71 @@ export class catchfish extends Component {
         this.gameStartView.active = true;
         this.timeInit();
         this.timeStart();
+        this.createFish();
     }
 
-    failGame(){
-        
+    private createFish(count:number=4){
+        if(this.fishParentNode && this.fishPrefab){
+            let len = count;
+
+            for(let i = 0; i < len; i++){
+                let fish = new Fish(this.fishPrefab);
+                fish.setParent(this.fishParentNode);
+                this.randomFish(fish);
+                this.fishs.push(fish);
+                this.moveFishes(fish,i*SHOOT_INTERVAL);
+            }
+        }
     }
+
+    private randomFish(fish:Fish){
+        let x = 1000;
+        let y = Math.random() * 700-200;
+        let spriteFramelen = this.spriteFrames.length;
+        let index = Math.floor(Math.random()*(spriteFramelen-1));
+        let spriteFrame = this.spriteFrames[index];
+        fish.setPosition(x,y);
+        fish.setSpriteFrame(spriteFrame);
+        const question = questions[Math.floor(Math.random() * questions.length)];
+        fish.setQuestion(question);
+        EventManager.getInstance().on(Fish.FishClick,this.selectFish,this);
+    }
+
+    private selectFish(fish,context){
+            // "question": "4 - 1 = ?",
+            // "options": ["3", "2", "5", "4"],
+            // "correctAnswer": "3"
+         if(context._curFish){
+             context._curFish.setSelect(context.unSelectColor,1);
+         }
+         context._curFish = fish;
+         let data = fish.getData();
+         let options = data.options;
+         let len = options.length;
+         for(let i = 0; i < len; i++){
+             let answer = options[i];
+             let wangNode = context.wangs[i];
+             let label = wangNode.getChildByName('Label').getComponent(Label);
+             label.string = answer;
+         }
+        context._curFish.setSelect(context.selectColor,2);
+    }
+
+    moveFishes(fish:Fish,delay:number = 0) {
+        if(fish.curTween)fish.curTween.stop();
+        // 使用 tween 创建运动效果
+        fish.curTween = tween(fish) // 对当前鱼对象进行 tween 动画
+            .delay(delay) // 每个对象延迟3秒开始
+            .to(15, { position: new Vec3(fish.position.x - 1600, fish.position.y, fish.position.z) })
+            .call(() => {
+                this.randomFish(fish);
+                this.moveFishes(fish);
+            })
+            .start(); // 启动动画
+
+    }
+
+
     update(deltaTime: number) {
         
     }
@@ -64,6 +179,64 @@ export class catchfish extends Component {
         const second = miao > 9 ? miao  : `0${miao}`
         this.Timer.string = `${fenzhong}:${second}`;
        
+    }
+
+    wangClick(event,data){
+        if(!this._curFish){
+            return;
+        }
+        let index = data;
+        let len = this.wangs.length;
+        for(let i = 0; i < len; i++){
+            if(i == index){
+                this.selectWang(i);
+            }else{
+                this.unSelectWang(i);
+            }
+        }
+
+        if(index == this._curFish.currentIndex){
+
+            let wangPrefab = instantiate(this.wangPrefab);
+            wangPrefab.setWorldScale(new Vec3(3,3,3));
+            let wang = this.wangs[index];
+            wang.addChild(wangPrefab);
+
+            tween(wangPrefab).to(1, { position: new Vec3(this._curFish.worldPosition.x-100, this._curFish.worldPosition.y-100, this._curFish.worldPosition.z) })
+                .call(() => {
+                    wang.removeChild(wangPrefab);
+                    this._curFish.setSelect(this.unSelectColor,1)
+                    this.randomFish(this._curFish);
+                    this.moveFishes(this._curFish);
+                })
+                .start(); // 启动动画
+
+
+            for(let i = 0; i < len; i++){
+                this.unSelectWang(i);
+            }
+        }
+
+    }
+
+    private selectWang(index:number){
+        let wang = this.wangs[index];
+        wang.getComponent(Sprite).color = this.selectColor;
+    }
+
+    private unSelectWang(index:number){
+        let wang = this.wangs[index];
+        wang.getComponent(Sprite).color = this.unSelectColor;
+    }
+
+    /**
+     * 返回应用大厅
+     */
+    quitGame(){
+        if(this.timerId !=null){
+            clearInterval(this.timerId);
+        }
+        SceneManager.getInstance().backToHall();
     }
 }
 
