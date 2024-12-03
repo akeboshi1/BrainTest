465736cda2baf130921b11411a41fb70c01b4372
akeboshi <akeboshi82@gmail.com>
2024-12-03 17:50:16 +0800

    登录界面人物序列图

--- assets/scripts/Game/UI/Login/LoginPanel.ts
+++ assets/scripts/Game/UI/Login/LoginPanel.ts
@@ -4,6 +4,7 @@ import {EventManager} from "../../../Core/Manager/Event/EventManager";
 import {LoginManager} from "../../../Core/Manager/LoginManager/LoginManager";
 import {PhoneLoginPanel} from "db://assets/scripts/Game/UI/Login/PhoneLoginPanel";
 import {UIManager} from "db://assets/scripts/Core/Manager/UI/UIManager";
+import {FrameComponent} from "db://assets/scripts/Core/Component/FrameComponent";
 const { ccclass, property } = _decorator;
 
 @ccclass('LoginPanel')
@@ -21,19 +22,15 @@ export class LoginPanel extends BasePanel {
     @property(Node)
     roleContainer:Node;
 
-    @property(Sprite)
-    roleSprite:Sprite;
-
-    @property(VideoPlayer)
-    video:VideoPlayer;
-
+    @property(FrameComponent)
+    roleFrameComponent:FrameComponent;
     // @property({tooltip: "这是序列帧数组", type: [SpriteFrame]})
     // spriteFrames:SpriteFrame[] = [];
 
     // @property({tooltip:"spine皮肤", type:sp.Skeleton })
     // spine: sp.Skeleton | null = null;
 
-    private canvas;
+    // private canvas;
     constructor() {
         super();
         LoginPanel.NAME ="LoginPanel";
@@ -45,7 +42,7 @@ export class LoginPanel extends BasePanel {
         EventManager.getInstance().on(eventName,this.loadPanelComplete,this);
         EventManager.getInstance().emit(eventName,eventName);
 
-
+        this.roleFrameComponent.playAnimation("idle",30);
 
         // let canvas = find("Canvas");
         // canvas.on(Node.EventType.TOUCH_END,this.playVideo,this)
@@ -53,27 +50,27 @@ export class LoginPanel extends BasePanel {
     }
 
     start(){
-        this.canvas = find("Canvas");
-        this.canvas.on(Node.EventType.TOUCH_END,this.playVideo,this);
+        // this.canvas = find("Canvas");
+        // this.canvas.on(Node.EventType.TOUCH_END,this.playVideo,this);
         //this.video.node.on(VideoPlayer.EventType.READY_TO_PLAY,this.playVideo,this);
     }
 
-    private playVideo(){
-        this.canvas.off(Node.EventType.TOUCH_END,this);
-        this.video.play();
-    }
-
-    resume(){
-        this.video.node.active = true;
-    }
-
-    onEnable() {
-        this.video.node.active = true;
-    }
-
-    onDisable() {
-      this.video.node.active = false;
-    }
+    // private playVideo(){
+    //     this.canvas.off(Node.EventType.TOUCH_END,this);
+    //     this.video.play();
+    // }
+    //
+    // resume(){
+    //     this.video.node.active = true;
+    // }
+    //
+    // onEnable() {
+    //     this.video.node.active = true;
+    // }
+    //
+    // onDisable() {
+    //   this.video.node.active = false;
+    // }
 
 
     update(deltaTime: number) {
@@ -90,21 +87,21 @@ export class LoginPanel extends BasePanel {
      * 点击协议显示协议面板
      */
     public xieyiClick() {
-        this.video.node.active = false;
+        //this.video.node.active = false;
         EventManager.getInstance().on(UIManager.BACK_TO_PARENT,this.backClick,this);
         LoginManager.getInstance().showXieyi(this.node);
     }
 
     public loginClick(){
-        this.video.node.active = false;
+       // this.video.node.active = false;
         EventManager.getInstance().on(UIManager.BACK_TO_PARENT,this.backClick,this);
         LoginManager.getInstance().showPhoneLoginPanel(this.node);
     }
 
     private backClick(){
         EventManager.getInstance().off(UIManager.BACK_TO_PARENT,this);
-        this.video.node.active = true;
-        this.video.play();
+        // this.video.node.active = true;
+        // this.video.play();
     }
 
     private loadPanelComplete(){
