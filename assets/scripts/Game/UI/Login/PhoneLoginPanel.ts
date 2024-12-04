551d1015d98800bcdaf76c251579d57fc59ddc52
akeboshi <akeboshi82@gmail.com>
2024-12-04 14:47:28 +0800

    app 手机验证码登录显示调整

--- assets/scripts/Game/UI/Login/PhoneLoginPanel.ts
+++ assets/scripts/Game/UI/Login/PhoneLoginPanel.ts
@@ -1,9 +1,10 @@
-import { _decorator, Button, EditBox,Node,sp } from 'cc';
+import { _decorator, Button, EditBox,Node,Sprite } from 'cc';
 import {BasePanel} from "../../../Core/UI/BasePanel";
 import {EventManager} from "../../..//Core/Manager/Event/EventManager";
 import {LoginManager} from "../../../Core/Manager/LoginManager/LoginManager";
 import {Global} from "../../../Core/Manager/Config/Global";
 import {UIManager} from "../../../Core/Manager/UI/UIManager";
+import {ColorUtil} from "db://assets/scripts/Core/Util/ColorUtil";
 const { ccclass, property } = _decorator;
 
 @ccclass('PhoneLoginPanel')
@@ -16,6 +17,9 @@ export class PhoneLoginPanel extends BasePanel{
 
     @property(EditBox)
      phoneNumberEdit:EditBox;
+
+    @property(Node)
+    phoneNumberEditBG:Node;
 
      constructor() {
          super();
@@ -30,7 +34,9 @@ export class PhoneLoginPanel extends BasePanel{
      }
 
      start() {
-         if(this.phoneNumberEdit.node)this.phoneNumberEdit.node.on(Node.EventType.TOUCH_END,this.checkBoxHandler,this);
+         if(this.phoneNumberEdit.node){
+             this.phoneNumberEdit.node.on(Node.EventType.TOUCH_END,this.checkBoxHandler,this);
+         }
      }
 
      onDisable() {
@@ -60,6 +66,21 @@ export class PhoneLoginPanel extends BasePanel{
     public enterClick(){
         Global.userData.phoneNumber = this.phoneNumberEdit.string;
         LoginManager.getInstance().showPhoneView(this.node);
+    }
+
+    /**
+     * editbox change
+     */
+    public textChange(){
+        let sprite = this.phoneNumberEditBG.getComponent(Sprite);
+        let btnSprite = this.enterBtn.node.getComponent(Sprite);
+        if(this.phoneNumberEdit.string.length>0){
+            sprite.color = ColorUtil.getCCColor(10,89,247);
+            btnSprite.color = ColorUtil.getCCColor(10,89,247);
+        }else{
+            sprite.color = ColorUtil.getCCColor(255,255,255);
+            btnSprite.color = ColorUtil.getCCColor(255,255,255);
+        }
     }
 
 
