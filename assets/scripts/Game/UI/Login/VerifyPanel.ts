6de270e4098dfdd17a6f23b743f883139b5717e6
akeboshi <akeboshi82@gmail.com>
2024-12-11 16:05:42 +0800

    邀请码ui

--- assets/scripts/Game/UI/Login/VerifyPanel.ts
+++ assets/scripts/Game/UI/Login/VerifyPanel.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, instantiate, Node ,Prefab,Label} from 'cc';
+import { _decorator, Component, tween, Node ,Prefab,Label} from 'cc';
 import {SceneManager} from "../../../Core/Manager/Scene/SceneManager";
 const { ccclass, property } = _decorator;
 
@@ -11,7 +11,12 @@ export class VerifyPanel extends Component{
     @property(Node)
     private verifyNode:Node;
 
+    @property(Node)
+    private loadEffect:Node;
+
     public static NAME:string = "VerifyPanel";
+
+    private _tween;
 
     start(){
 
@@ -19,7 +24,25 @@ export class VerifyPanel extends Component{
 
 
     submit(){
+         this.verifyNode.active = false;
+         this.loadNode.active = true;
+         let self = this;
+        this._tween = tween(this.loadEffect)
+            .to(3, { angle: this.loadEffect.angle + 360 })
+            .to(3, { angle: this.loadEffect.angle + 720 })
+            .to(3, { angle: this.loadEffect.angle + 1080 })// 9秒内旋转3圈
+            .call(() => {
+                // 在 tween 完成后的回调中停止 tween
+                console.log("旋转结束，停止 tween");
+                self.close();
+                self._tween.stop(); // 停止 tween
+            })
+            .start();
 
+    }
+
+    close(){
+        SceneManager.getInstance().backToHall();
     }
 
     useCamera(){
