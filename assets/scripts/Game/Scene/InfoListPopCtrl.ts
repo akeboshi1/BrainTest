12d9451d9c9754d164eb6c5b78a00ee275789ad6
miokky <iamtbfeng@outlook.com>
2024-12-17 18:42:08 +0800

    Update InfoListPopCtrl.ts

--- assets/scripts/Game/Scene/InfoListPopCtrl.ts
+++ assets/scripts/Game/Scene/InfoListPopCtrl.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Node, Prefab, instantiate, Label } from 'cc';
+import { _decorator, Component, Node, Prefab, instantiate, Label, Button } from 'cc';
 import { EventManager } from '../../Core/Manager/Event/EventManager';
 import { TaskManager } from '../Task/TaskManager';
 import { SkewersManager } from '../Task/Skewers/SkewersManager';
@@ -15,54 +15,52 @@ export class InfoListPopCtrl extends Component {
     @property(Node)
     parentNode: Node = null;
 
-    private static _infoDic: Map<number, Node>=new Map();
+
+    private _taskID: number = -1;
+
     start() {
     }
 
     update(deltaTime: number) {
 
     }
-    updateInfoList(InfoData) {
-        let alertPrefab=null;
-        if(InfoData.notification_type=="1"){
-            alertPrefab = instantiate(this.taskAlertPrefab);
-            let btn1 = alertPrefab.getChildByName('btn1');
-            btn1["sub_id"] = InfoData.sub_id;
-            btn1.on('click', this.gotaskList,this);
-            let btn2 = alertPrefab.getChildByName('btn2');
-            btn2["id"] = InfoData.id;
-            btn2.on('click', this.hideInfoAlert,this);
-        }else if(InfoData.notification_type=="2"){
-            alertPrefab = instantiate(this.remindAlertPrefab);
-            let btn1 = alertPrefab.getChildByName('btn1');
-            btn1["id"] = InfoData.id;
-            btn1.on('click', this.hideInfoAlert,this);
-        }
-        alertPrefab.parent = this.parentNode;
-        alertPrefab.getChildByName('ScrollView').getChildByName('view').getChildByName('content').getChildByName('item').getComponent(Label).string = InfoData.content;
-        InfoListPopCtrl._infoDic.set(InfoData.id, alertPrefab);
-       
-       
-    }
-    private _taskID:number = -1;
-    gotaskList(event) {
-        event.target.off('click', this.gotaskList);
-        this._taskID = Number(event.target["sub_id"]);
-        EventManager.getInstance().on(SkewersManager.TASK_GET_BRAIN_TRAININGS,this.gotaskListCallBack,this);
-        SkewersManager.getInstance().requestBranisTraining_list(this._taskID)
-    }
 
-    private gotaskListCallBack(){
-        EventManager.getInstance().off(SkewersManager.TASK_GET_BRAIN_TRAININGS,this);
+    private gotaskListCallBack() {
+        EventManager.getInstance().off(SkewersManager.TASK_GET_BRAIN_TRAININGS, this);
         TaskManager.getInstance().requestStartTask(this._taskID);
     }
 
-    hideInfoAlert(event) {
-        event.target.off('click', this.hideInfoAlert);
-        let alertNode = InfoListPopCtrl._infoDic.get(event.target._id);
-        this.node.getChildByName('ScrollView').getChildByName('view').getChildByName('content').removeChild(alertNode);
-        InfoListPopCtrl._infoDic.delete(event.target._id);
-        if(InfoListPopCtrl._infoDic.size==0){
+    updateInfoList(InfoData) {
+        let alertPrefab = null;
+        if (InfoData.notification_type == "1") {
+            alertPrefab = instantiate(this.taskAlertPrefab);
+            let btn1 = alertPrefab.getChildByName('btn1');
+            btn1["sub_id"] = InfoData.sub_id;
+            btn1.on('click', this.gotaskList, this);
+            let btn2 = alertPrefab.getChildByName('btn2');
+            btn2["id"] = InfoData.id;
+            btn2.on('click', this.hideInfoAlert.bind(this,alertPrefab), this);
+        } else if (InfoData.notification_type == "2") {
+            alertPrefab = instantiate(this.remindAlertPrefab);
+            let btn1: Node = alertPrefab.getChildByName('btn1');
+            btn1["id"] = InfoData.id;
+            btn1.on('click', this.hideInfoAlert.bind(this,alertPrefab), this);
+        }
+        alertPrefab.parent = this.parentNode;
+        alertPrefab.getChildByName('ScrollView').getChildByName('view').getChildByName('content').getChildByName('item').getComponent(Label).string = InfoData.content;
+    }
+   
+    gotaskList(event) {
+        event.target.off('click', this.gotaskList);
+        this._taskID = Number(event.target["sub_id"]);
+        EventManager.getInstance().on(SkewersManager.TASK_GET_BRAIN_TRAININGS, this.gotaskListCallBack, this);
+        SkewersManager.getInstance().requestBranisTraining_list(this._taskID)
+    }
+
+    hideInfoAlert(infoItem:Node) {
+        let content: Node = this.node.getChildByName('ScrollView').getChildByName('view').getChildByName('content');
+        infoItem.removeFromParent();
+        if (content.children.length == 0) {
             EventManager.getInstance().emit('hideInfoListPop');
         }
     }
