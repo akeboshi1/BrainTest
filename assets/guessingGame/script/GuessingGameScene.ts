fdb892e98dbed502a6fa9e1947e19dc6099f0e86
akeboshi <akeboshi82@gmail.com>
2024-12-18 17:39:47 +0800

    猜谜失败界面

--- assets/guessingGame/script/GuessingGameScene.ts
+++ assets/guessingGame/script/GuessingGameScene.ts
@@ -164,15 +164,23 @@ export class GuessingGameScene extends Component {
     }
 
     private processAnswer(ans:string = null){
-        this.timerRT.resumeTimer();
+        this.pauseTime();
 
         const result:boolean = ans && this.currentQuestion.answer == ans;
         this.resultPanel.active = true;
         if(Global.isSkewersGame){
-
-            this.requestGameResult(true)
+            this.successTextNode.active = false;
+            this.failedTextNode.active = false;
+            this.requestGameResult(result);
             if(SkewersManager.getInstance().isRunOver()){
                 SkewersManager.getInstance().showGameAlert(this.viewNode,AlertType.Sucess_Big,"太棒了，恭喜你全部通关","收获xxx点脑力值！",0,0,null,this.exitCallBack,this);
+                return;
+            }
+            if(!result){
+                let trainData = SkewersManager.getInstance().getUnCompleteGameData();
+                let maxCount = SkewersManager.getInstance().getGameCount();
+                let curCount = trainData.seq<0?0:trainData.seq;
+                SkewersManager.getInstance().showGameAlert(this.viewNode,AlertType.Normal, "真遗憾，请加油！",'',curCount,maxCount,this.onClickGotoNextlevel1,this.exitCallBack,this);
                 return;
             }
             EventManager.getInstance().on(SkewersManager.REQUEST_SKEWERSGAME_COMPLETE,this.requestSkewersGameComplete,this);
@@ -190,7 +198,7 @@ export class GuessingGameScene extends Component {
         let curCount = trainData.seq;
         // 游戏内界面提示
         if(maxCount != curCount){
-            SkewersManager.getInstance().showGameAlert(this.viewNode,AlertType.Normal,"太棒了，请继续！","",curCount,maxCount,this.onClickGotoNextlevel,this.exitCallBack,this);
+            SkewersManager.getInstance().showGameAlert(this.viewNode,AlertType.Normal,"太棒了，请继续！","",curCount,maxCount,this.onClickGotoNextlevel1,this.exitCallBack,this);
         }else{
             if (!SkewersManager.getInstance().isRunOver()) {
                 SkewersManager.getInstance().showGameAlert(this.viewNode,AlertType.Sucess_Small,"太棒了，恭喜你通关猜谜游戏","收获xxx点脑力值！",0,0,this.nextAlertHandler,this.exitCallBack,this);
@@ -233,6 +241,18 @@ export class GuessingGameScene extends Component {
         // }
         // 下一关
        this.onClickContinueGame();
+    }
+
+    onClickGotoNextlevel1(context){
+        if(Global.isSkewersGame) {
+            if(SkewersManager.getInstance().isRunOver()){
+                SkewersManager.getInstance().exitCallBack();
+            }else{
+                context.onClickGotoNextlevel();
+            }
+        }else{
+            // GameCenterManager.getInstance().exitCallBack();
+        }
     }
 
     onClickReplay(){
