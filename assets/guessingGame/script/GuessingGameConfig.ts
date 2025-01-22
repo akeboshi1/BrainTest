5e1c19e598546a2ea31dec3db8089e449768d695
miokky <iamtbfeng@outlook.com>
2025-01-22 17:44:21 +0800

    猜谜出题序列bug修复

--- assets/guessingGame/script/GuessingGameConfig.ts
+++ assets/guessingGame/script/GuessingGameConfig.ts
@@ -1,5 +1,4 @@
 import { assetManager, JsonAsset } from "cc";
-import { ConfigManager } from "../../scripts/Core/Manager/Config/ConfigManager";
 import { DebugLog } from "../../scripts/Core/Util/DebugLog";
 
 export class GuessingQuestion {
@@ -19,11 +18,10 @@ export class GuessingQuestion {
 
 export class GuessingGameConfig {
     private quessingQuestions: Map<string, GuessingQuestion> = new Map();
-    private questionList: GuessingQuestion[] = [];
     private jsonFilePath: string = "config/guessingGameConfig";
     private bundleName: string = "guessingGame";
+    private questionNums: number[] = [];
 
-    private _unAnswerList:GuessingQuestion[] = [];
     // 解析JSON文件的方法
     async loadConfig() {
         let bundle = assetManager.getBundle(this.bundleName);
@@ -31,7 +29,7 @@ export class GuessingGameConfig {
         await new Promise((resolve, reject) => {
             bundle.load(this.jsonFilePath, JsonAsset, (err: Error | null, data: JsonAsset) => {
                 if (err) {
-                    DebugLog.instance.warn("加载配置文件失败:"+err);
+                    DebugLog.instance.warn("加载配置文件失败:" + err);
                     reject(err);
                 } else {
                     let qb = data.json.questionBank;
@@ -39,9 +37,8 @@ export class GuessingGameConfig {
                     for (let question of qb) {
                         question.hasAnswer = false;
                         this.quessingQuestions.set(question.questionNumber.toString(), question);
+                        this.questionNums.push(question.questionNumber);
                     }
-                    this.questionList = Array.from(this.quessingQuestions.values());
-                    this._unAnswerList = Array.from(this.quessingQuestions.values());
                     DebugLog.instance.log("GuessingGameConfig load success!!! ");
                     resolve(data);
                 }
@@ -50,15 +47,11 @@ export class GuessingGameConfig {
     }
 
     // 根据题目编号获取指定题目信息
-    getQuestionByNumber(questionNumber: number,isRandom:boolean = false): GuessingQuestion {
-        if(!isRandom){
-            if (!this.quessingQuestions.has(questionNumber.toString())) {
-                throw new Error(`未找到题目编号为 ${questionNumber} 的题目信息，请检查配置文件或题目编号是否正确。`);
-            }
-            return this.quessingQuestions.get(questionNumber.toString())!;
+    getQuestionByNumber(questionNumber: number): GuessingQuestion {
+        if (!this.quessingQuestions.has(questionNumber.toString())) {
+            throw new Error(`未找到题目编号为 ${questionNumber} 的题目信息，请检查配置文件或题目编号是否正确。`);
         }
-
-        return this.getUnAnswerQuestion(questionNumber);
+        return this.quessingQuestions.get(questionNumber.toString())!;
     }
 
     // 根据题目编号获取题目答案
@@ -74,45 +67,17 @@ export class GuessingGameConfig {
     }
 
     // 根据题目编号获取音频资源文件名
-    getAudioSourceByNumber(questionNumber: number,isRandom:boolean = false): string {
-        const question = isRandom == false?this.getQuestionByNumber(questionNumber):this.getUnAnswerQuestion(questionNumber);
+    getAudioSourceByNumber(questionNumber: number): string {
+        const question = this.getQuestionByNumber(questionNumber);
         return question.audioSource;
     }
 
-    getNextQuestionNumber(questionNumber:number):number{
-        const questionNumbers = Array.from(this.quessingQuestions.keys()).map(Number);
-        const currentIndex = questionNumbers.indexOf(questionNumber);
-        if (currentIndex === -1) {
-            throw new Error(`题目编号 ${questionNumber} 不存在，请检查输入的题目编号是否正确。`);
+    formartQuestionID(v:number):number{
+        if(this.questionNums.indexOf(v)>=0){
+            return v;
+        }else{
+            v = v % this.questionNums.length + 1;
+            return v;
         }
-
-        //设置当前question已经被回答过了
-        this.updateAnswerdQuestion(questionNumber);
-
-        const nextIndex = currentIndex + 1;
-        if (nextIndex >= questionNumbers.length) {
-            return questionNumbers[0]; // 如果已经是最后一题，返回第一道题的编号，形成循环
-        }
-        return questionNumbers[nextIndex];
-    }
-
-    updateAnswerdQuestion(questionNumber:number):GuessingQuestion {
-         let question = this.getQuestionByNumber(questionNumber);
-         question.hasAnswer = true;
-         this._unAnswerList = this.questionList.filter(question => !question.hasAnswer);
-         return question;
-    }
-
-    getUnAnswerQuestionIndex():number{
-        if(this._unAnswerList.length<1) {
-            DebugLog.instance.log("猜谜题库已经全部答完");
-            return null;
-        }
-        return Math.floor(Math.random() * this._unAnswerList.length);
-    }
-
-    // 根据题目编号获取没有回答过的题目信息
-    getUnAnswerQuestion(questionNumber: number): GuessingQuestion {
-        return this._unAnswerList[questionNumber];
     }
 }
