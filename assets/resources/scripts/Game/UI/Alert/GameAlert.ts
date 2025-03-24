74d323eff9aa6f951f1508d640fcfaeee598658d
akeboshi <akeboshi82@gmail.com>
2025-03-24 10:11:20 +0800

    屏蔽脑力值文本

--- assets/resources/scripts/Game/UI/Alert/GameAlert.ts
+++ assets/resources/scripts/Game/UI/Alert/GameAlert.ts
@@ -155,14 +155,14 @@ export class GameAlert extends Component {
                     })
                     .start();
                 this.iconConNode.active = true;
-                this.decLabel.node.active = true;
+                this.decLabel.node.active = false;
                 this.progressBar.node.active = false;
                 this.playAudio("music/cheer", true);
                 startBtnUITransform.width = 250;
                 break;
             case AlertType.Sucess_Big:
                 this.startBtn.node.active = true;
-                this.decLabel.node.active = true;
+                this.decLabel.node.active = false;
                 this.titleLabel.node.active = true;
                 this.progressBar.node.active = false;
                 this.iconConNode.active = false;
