72bae12e5b8975d387b0eedd1b9637c104b06aff
miokky <iamtbfeng@outlook.com>
2025-03-28 11:59:03 +0800

    回滚猜谜游戏玩法，保留了串烧和语言接口的改动
    Revert "修改计时器停止规则"
    
    This reverts commit 03912aa7c420f6e36b0db00ceff203637264450f.
    
    Revert "去掉了清除答案功能"
    
    This reverts commit a34c2ee473a4f0c6e116b830bb790af19ff61f04.
    
    Revert "猜谜界面调整"
    
    This reverts commit 447df2c809881c3742bbd2d9b1b2099219ffe421.

--- assets/guessingGame/script/GuessingGameConfig.ts
+++ assets/guessingGame/script/GuessingGameConfig.ts
@@ -4,10 +4,15 @@ import { DebugLog } from "../../resources/scripts/Core/Util/DebugLog";
 export class GuessingQuestion {
     questionNumber: number;
     questionText: string;
-    answer: string[];
+    options: {
+        a: string;
+        b: string;
+        c: string;
+        d: string;
+    };
+    answer: string;
     solutionThoughts: string;
     audioSource: string;
-    tips: string;
     hasAnswer: boolean;
 }
 
@@ -50,7 +55,7 @@ export class GuessingGameConfig {
     }
 
     // 根据题目编号获取题目答案
-    getAnswerByNumber(questionNumber: number): string[] {
+    getAnswerByNumber(questionNumber: number): string {
         const question = this.getQuestionByNumber(questionNumber);
         return question.answer;
     }
