d80269c27d544f6c51fe9a4db79465da2b151aec
akeboshi <akeboshi82@gmail.com>
2025-04-11 15:42:55 +0800

    游戏大厅麻将弹窗坐标调整

--- assets/sentenceMaking/script/SentenceMakingScene.ts
+++ assets/sentenceMaking/script/SentenceMakingScene.ts
@@ -112,7 +112,7 @@ export class SentenceMakingScene extends BaseScene<IBaseGameChild> {
             ad.confirmCb = () => {
                 this.exitCallBack(this);
             };
-            ad.y = 350; // 设置y坐标
+            // ad.y = 350; // 设置y坐标
             AlertManager.getInstance().showAlert(ad);
         });
     }
@@ -221,7 +221,7 @@ export class SentenceMakingScene extends BaseScene<IBaseGameChild> {
         ad.confirmCb = () => {
             this.startGameFlow();
         };
-        ad.y = 350; // 设置y坐标
+        // ad.y = 350; // 设置y坐标
         AlertManager.getInstance().showAlert(ad);
     }
 
@@ -647,7 +647,7 @@ export class SentenceMakingScene extends BaseScene<IBaseGameChild> {
         let isSuccess = true;
         let wrongIndices = [];
         let ad: AlertData = new AlertData();
-        ad.y = 350; // 设置y坐标
+        // ad.y = 350; // 设置y坐标
 
 
         ad.cancelButtonVisible = false;
@@ -796,7 +796,7 @@ export class SentenceMakingScene extends BaseScene<IBaseGameChild> {
             ad.cancelButtonVisible = false;
             ad.title = "没有时间啦";
             ad.message = "挑战失败";
-            ad.y = 350; // 设置y坐标
+            // ad.y = 350; // 设置y坐标
             AlertManager.getInstance().showAlert(ad);
         }
 
