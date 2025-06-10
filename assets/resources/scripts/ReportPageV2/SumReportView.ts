b1669576c4bb67eb6de8000da96be0b26ef06368
akeboshi <akeboshi82@gmail.com>
2025-06-10 18:25:16 +0800

    邀请码按钮颜色调整 + 个人中心预制体回退

--- assets/resources/scripts/ReportPageV2/SumReportView.ts
+++ assets/resources/scripts/ReportPageV2/SumReportView.ts
@@ -1,6 +1,6 @@
 import { _decorator, Component, Label, Node } from 'cc';
-import { RadiaGraph } from '../IndexPageV2/RadiaGraph';
 import { ReportData, ReportManager } from '../ManagerV2/ReportManager';
+import {RadiaGraph} from "db://assets/resources/scripts/indexPageV2/RadiaGraph";
 const { ccclass, property } = _decorator;
 
 @ccclass('SumReportView')
