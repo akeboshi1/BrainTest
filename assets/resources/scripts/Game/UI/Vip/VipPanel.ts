86218537a64e6761408f0cac8770fa56effd5547
akeboshi <akeboshi82@gmail.com>
2025-06-16 18:08:56 +0800

    会员界面

---
+++ assets/resources/scripts/Game/UI/Vip/VipPanel.ts
@@ -0,0 +1,33 @@
+import {BasePanel} from "db://assets/resources/scripts/Core/UI/BasePanel";
+
+export class VipPanel extends BasePanel {
+
+
+
+
+
+    public static NAME: string = "VipPanel";
+
+    constructor() {
+        super();
+        this.name = VipPanel.NAME;
+    }
+
+    onLoad(): void {
+    }
+
+    start(){
+    }
+
+    onEnable(): void {
+    }
+
+    onDisable(): void {
+    }
+
+    onDestroy(): void {
+    }
+
+
+
+}
