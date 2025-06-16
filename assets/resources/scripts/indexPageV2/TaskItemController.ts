b8ec8513370c46316adc0e8a4f011179cc01674d
赵邦宁 <2922886063@qq.com>
2025-06-16 16:12:30 +0800

    添加任务点击事件

--- assets/resources/scripts/indexPageV2/TaskItemController.ts
+++ assets/resources/scripts/indexPageV2/TaskItemController.ts
@@ -1,6 +1,9 @@
 import { _decorator, Component, Label, Node, Sprite, SpriteFrame, resources } from 'cc';
 
 import { DebugLog } from '../Core/Util/DebugLog';
+import { UIManager } from '../Core/Manager/UI/UIManager';
+import { TaskAndNotificationPanelCtrl } from '../Game/UI/TaskAndNotificationPanel/TaskAndNotificationPanelCtrl';
+import { BundleName } from '../Core/Manager/Load/BundleName';
 const { ccclass, property } = _decorator;
 
 @ccclass('TaskItemController')
@@ -15,10 +18,41 @@ export class TaskItemController extends Component {
     private taskIcon: Sprite = null;
     @property(Label)
     private buttonText: Label = null;
+    @property(Node)
+    private button: Node = null;
+    private taskIndex: number = -1;
 
-    public onFirstTaskClick: (taskController: TaskItemController) => void;
-    public onOtherTaskClick: (index: number, taskController: TaskItemController) => void;
+    onEnable() {
+        // 添加按钮点击事件监听
+        if (this.button) {
+            this.button.on(Node.EventType.TOUCH_END, this.onButtonClick, this);
+        }
+    }
+    onDisable() {
+        // 移除事件监听
+        if (this.button) {
+            this.button.off(Node.EventType.TOUCH_END, this.onButtonClick, this);
+        }
+    }
 
+    private onButtonClick() {
+        if (this.taskIndex==0) {
+            this.onFirstTaskClick();
+        } else if (this.taskIndex==1) {
+            this.onOtherTaskClick();
+        }
+    }
+    onFirstTaskClick() {
+        UIManager.getInstance().registerPanel(TaskAndNotificationPanelCtrl.NAME, BundleName.RESOURCES, "prefab/TaskAndNotification/TaskAndNotificationPanel", TaskAndNotificationPanelCtrl);
+        UIManager.getInstance().showPanel(TaskAndNotificationPanelCtrl.NAME);
+    }
+    onOtherTaskClick() {
+        console.log(`Task ${this.taskIndex} clicked`);
+    }
+
+    setTaskIndex(index: number) {
+        this.taskIndex = index;
+    }
 
     setTaskTitle(title: string) {
         this.taskTitle.string = title;
