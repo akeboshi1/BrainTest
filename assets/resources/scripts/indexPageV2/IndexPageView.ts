808cf3e03c4e70f17c4cff34a320c6e0ba540320
赵邦宁 <2922886063@qq.com>
2025-06-27 11:35:58 +0800

    手指操入口取消监听

--- assets/resources/scripts/indexPageV2/IndexPageView.ts
+++ assets/resources/scripts/indexPageV2/IndexPageView.ts
@@ -53,7 +53,7 @@ export class IndexPageView extends Component {
         PersonalCenterManager.getInstance().requestUserInfo();
     }
     onEnable() {
-        EventManager.getInstance().off(BundlePreloadEvent.FINISH, this);
+       
         EventManager.getInstance().on(PersonalCenterManager.getUserInfoCallBack, this.getUserInfoCallBack, this);
         EventManager.getInstance().on(ReportManager.getBrainTrainingTiersCallback, this.getBrainTrainingTiersCallback, this); 
 
@@ -61,6 +61,7 @@ export class IndexPageView extends Component {
     onDisable() {
         EventManager.getInstance().off(PersonalCenterManager.getUserInfoCallBack, this);
         EventManager.getInstance().off(ReportManager.getBrainTrainingTiersCallback, this);      
+        EventManager.getInstance().off(BundlePreloadEvent.FINISH, this);
     }
     // 获取大脑训练等级回调函数
     getBrainTrainingTiersCallback() {
