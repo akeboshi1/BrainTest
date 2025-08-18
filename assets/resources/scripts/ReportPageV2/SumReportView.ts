d6c6861e28ac0399f10024f965728a78eb5b9c0d
miokky <iamtbfeng@outlook.com>
2025-08-18 18:20:18 +0800

    报告界面文本优化

--- assets/resources/scripts/ReportPageV2/SumReportView.ts
+++ assets/resources/scripts/ReportPageV2/SumReportView.ts
@@ -14,36 +14,42 @@ export class SumReportView extends Component {
 
     @property(Label)
     weekStatistics: Label = null;
-    
-    start() {   
+
+    start() {
         ReportManager.getInstance().weekStatistics.addListener(this.onWeekStatisticsChange.bind(this));
         ReportManager.getInstance().reportDataList.addListener(this.onReportDataListChange.bind(this));
         ReportManager.getInstance().reportDataListInitial.addListener(this.onReportDataListInitialChange.bind(this));
     }
-    
+
     protected onDestroy(): void {
         ReportManager.getInstance().weekStatistics.removeListener(this.onWeekStatisticsChange.bind(this));
         ReportManager.getInstance().reportDataList.removeListener(this.onReportDataListChange.bind(this));
         ReportManager.getInstance().reportDataListInitial.removeListener(this.onReportDataListInitialChange.bind(this));
     }
-    
-    
-    onWeekStatisticsChange(data: WeekStatisticsData){
-        this.weekStatistics.string = `统计周期${data.start_date?data.start_date:'——'}至${data.end_date?data.end_date:'——'}`;
+
+
+    onWeekStatisticsChange(data: WeekStatisticsData) {
+        this.weekStatistics.string = `统计周期${data.start_date ? this.processDateString(data.start_date) : '——'}至${data.end_date ? this.processDateString(data.end_date) : '——'}`;
     }
+    private processDateString(date: string): string {
+        let year = date.split('-')[0];
+        let month = date.split('-')[1];
+        let day = date.split('-')[2];
+        return `${month}.${day}`;
+    }
+
 
     onReportDataListChange(data: ReportData[]) {
         const values = data.map(item => item.tier);
         this.radarMap.getComponent(RadiaGraph).setValues(values);
         this.radarMap.getComponent(RadiaGraph).updateView(data);
     }
-    
+
     onReportDataListInitialChange(data: ReportData[]) {
         const valuesInitial = data.map(item => item.tier);
         this.radarMap.getComponent(RadiaGraph).setSecondValues(valuesInitial);
-        this.radarMap.getComponent(RadiaGraph).updateView(data);
     }
-   
+
     clickNavBar(event, data) {
         let userData = PersonalCenterManager.getInstance().userInfoData;
         if (!userData.has_initial_tier) {
@@ -51,7 +57,7 @@ export class SumReportView extends Component {
         }
         EventManager.getInstance().emit('onTopNavBarClick', data);
     }
-    
+
 }
 
 
