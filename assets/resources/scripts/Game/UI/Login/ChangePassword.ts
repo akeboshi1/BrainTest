30e98b10f6238c877f27e646d225bbc829387357
akeboshi <akeboshi82@gmail.com>
2025-08-26 16:24:05 +0800

    前端验证密码格式

--- assets/resources/scripts/Game/UI/Login/ChangePassword.ts
+++ assets/resources/scripts/Game/UI/Login/ChangePassword.ts
@@ -58,44 +58,44 @@ export class ChangePassword extends BasePanel {
     }
 
     public confirmHandler() {
-        // let errorMessage = "";
+        let errorMessage = "";
 
-        // // 验证密码只能包含数字和字母
-        // const passwordRegex = /^[a-zA-Z0-9]+$/;
-        // if (this.oldPassword == null || this.oldPassword == "") {
-        //     errorMessage = "请输入旧密码";
-        // }
-        // else if (!passwordRegex.test(this.newPassword)) {
-        //     errorMessage = "密码只能包含数字和字母";
-        // }
-        // else if (this.oldPassword.length < 4 || this.oldPassword.length > 10) {
-        //     errorMessage = "密码长度为4-10位";
-        // }
-        // else if (this.newPassword == null || this.newPassword == "") {
-        //     errorMessage = "请输入新密码";
-        // }
-        // else if (this.confirmPassword == null || this.confirmPassword == "") {
-        //     errorMessage = "请输入确认密码";
-        // }
-        // else if (this.newPassword != this.confirmPassword) {
-        //     errorMessage = "两次输入的密码不一致";
-        // }
-        // else if (this.newPassword.length < 4 || this.newPassword.length > 10) {
-        //     errorMessage = "密码长度为4-10位";
-        // }
-        // else if (this.oldPassword == this.newPassword) {
-        //     errorMessage = "新密码不能与旧密码相同";
-        // }
+        // 验证密码只能包含数字和字母
+        const passwordRegex = /^[a-zA-Z0-9]+$/;
+        if (this.oldPassword == null || this.oldPassword == "") {
+            errorMessage = "请输入旧密码";
+        }
+        else if (this.oldPassword.length < 4 || this.oldPassword.length > 10) {
+            errorMessage = "密码长度为4-10位";
+        }
+        else if (this.newPassword == null || this.newPassword == "") {
+            errorMessage = "请输入新密码";
+        }
+        else if (!passwordRegex.test(this.newPassword)) {
+            errorMessage = "密码只能包含数字和字母";
+        }
+        else if (this.confirmPassword == null || this.confirmPassword == "") {
+            errorMessage = "请输入确认密码";
+        }
+        else if (this.newPassword != this.confirmPassword) {
+            errorMessage = "两次输入的密码不一致";
+        }
+        else if (this.newPassword.length < 4 || this.newPassword.length > 10) {
+            errorMessage = "密码长度为4-10位";
+        }
+        else if (this.oldPassword == this.newPassword) {
+            errorMessage = "新密码不能与旧密码相同";
+        }
 
-        // if (errorMessage != "") {
-        //     const alertData: AlertData = new AlertData();
-        //     alertData.confirmCb = function () {
-        //         AlertManager.getInstance().closeCurrentAlert();
-        //     }.bind(this);
-        //     alertData.message = errorMessage;
-        //     AlertManager.getInstance().showAlert(alertData);
-        //     return;
-        // }
+        if (errorMessage != "") {
+            const alertData: AlertData = new AlertData();
+            alertData.confirmCb = function () {
+                AlertManager.getInstance().closeCurrentAlert();
+            }.bind(this);
+            alertData.message = errorMessage;
+            AlertManager.getInstance().showAlert(alertData);
+            return;
+        }
 
 
         EventManager.getInstance().on(ChangePassword.updatePasswordResult, this.updatePasswordResultHandler, this, true);
