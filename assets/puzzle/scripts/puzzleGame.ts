174a055068f179015e94c2a84dce8d6913901e11
akeboshi <akeboshi82@gmail.com>
2025-09-05 16:22:42 +0800

    拼图 302，322，328资源替换

--- assets/puzzle/scripts/puzzleGame.ts
+++ assets/puzzle/scripts/puzzleGame.ts
@@ -154,7 +154,7 @@ export class puzzleGame extends BaseScene<IBaseGameChild> {
         });
     }
 
-    private textureLen = 469;
+    private textureLen = 466;
     start() {
         super.start();
         for (let i = 1; i < this.textureLen; i++) {
