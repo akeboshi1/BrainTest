f1186b5c226974a7e8871cdd2016d4af69a8e929
miokky <iamtbfeng@outlook.com>
2025-09-19 11:53:57 +0800

    远程资源少量测试文件

---
+++ assets/resources/resource-manifest.ts
@@ -0,0 +1,8 @@
+// 资源清单文件
+// 此文件由resource-processor自动生成
+
+export enum ImagesManifest {
+  DEFAULT_DEMO_PNG = 'default/d71e8469e0eed18234696f741ab7eb9b.png',
+  ZHONGQIU_DEMO_PNG = 'zhongqiu/d71e8469e0eed18234696f741ab7eb9b.png'
+}
+
