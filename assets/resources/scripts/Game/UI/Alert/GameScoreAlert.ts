62171bdd7caf823f5a3cbe451d110f9d41f2b8a8
akeboshi <akeboshi82@gmail.com>
2025-10-30 15:28:21 +0800

    积分界面文字调整

--- assets/resources/scripts/Game/UI/Alert/GameScoreAlert.ts
+++ assets/resources/scripts/Game/UI/Alert/GameScoreAlert.ts
@@ -153,7 +153,7 @@ export class GameScoreAlert extends BasePanel {
             }
         }
 
-        let totalScore = len*100;
+        let totalScore = len * 100;
         this.totalScoreLabel1.string = `(满分:${totalScore}分)`;
 
         // 延迟显示并播放敲图章动画
