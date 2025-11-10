70c6d8580aca199444b5b3d5fb022f8da4eb2dc6
akeboshi <akeboshi82@gmail.com>
2025-11-10 17:34:54 +0800

    打开聊天音效log

--- assets/resources/scripts/Game/UI/ChatPanel/Model/ChatModel.ts
+++ assets/resources/scripts/Game/UI/ChatPanel/Model/ChatModel.ts
@@ -315,14 +315,14 @@ export class ChatModel {
 
     public pauseMusic(): void {
         if (sys.platform === 'ANDROID') {
-            DebugLog.instance.log('ChatModel: 暂停歌曲');
+            DebugLog.instance.warn('ChatModel: 暂停歌曲');
             native.bridge.sendToNative(NativeEvent.CHAT_SONG_PAUSE, JSON.stringify({}));
         }
     }
 
     public resumeMusic(): void {
         if (sys.platform === 'ANDROID') {
-            DebugLog.instance.log('ChatModel: 恢复歌曲');
+            DebugLog.instance.warn('ChatModel: 恢复歌曲');
             native.bridge.sendToNative(NativeEvent.CHAT_SONG_RESUME, JSON.stringify({}));
         }
     }
@@ -580,7 +580,7 @@ export class ChatModel {
         if (sys.platform === 'ANDROID') {
             const song = this._pendingSong;
             this._pendingSong = null;
-            DebugLog.instance.log('ChatModel: 播放歌曲', song);
+            DebugLog.instance.warn('ChatModel: 播放歌曲', song);
             native.bridge.sendToNative(NativeEvent.CHAT_MODE_SWITCH, JSON.stringify({
                 "mode": "song",
                 "songName": song.name,
