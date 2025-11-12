8405a82245c877d0afc9b302b75978bf8d2916e9
akeboshi <akeboshi82@gmail.com>
2025-11-12 16:31:05 +0800

    切换数字人时做判断

--- assets/resources/scripts/Game/UI/ChatPanel/Model/ChatModel.ts
+++ assets/resources/scripts/Game/UI/ChatPanel/Model/ChatModel.ts
@@ -533,11 +533,11 @@ export class ChatModel {
      * 切换数字人角色
      */
     switchCharactor(){
-         // 如果没有收到上次切换完成的事件，则不能再次切换
-         if (!this._canSwitchCharactor) {
-            console.log('ChatModel: 上次切换未完成，无法再次切换数字人');
-            return;
-        }
+        //  // 如果没有收到上次切换完成的事件，则不能再次切换
+        //  if (!this._canSwitchCharactor) {
+        //     console.log('ChatModel: 上次切换未完成，无法再次切换数字人');
+        //     return;
+        // }
 
         // 如果这次切换的数字人id和上一次切换的数字人id一样，则直接返回
         if (this._selectedCharactorId === this._lastSwitchedCharactorId) {
